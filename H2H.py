c722c4deb4f01d3f15f89d2c4d1bb031ef1c6ef7
alexmerryman <alexandermerryman2018@u.northwestern.edu>
2017-05-17 20:09:15 -0500

    Added notes.txt as key to data

--- H2H.py
+++ H2H.py
@@ -8,8 +8,16 @@ Created on Tue May 16 12:55:28 2017
 
 import pandas as pd
 import numpy as np
+import os
 
-atp_players = pd.read_csv('atp_players.csv')
+cwd = os.getcwd()
+ATP_data_dir = cwd + '/data/tennis_ATP'
+os.chdir(ATP_data_dir)
+#print os.listdir(ATP_data_dir)
+
+atp_players = pd.read_csv('atp_players.csv', names=['player_id', 'last_name',
+                                                    'first_name', 'hand',
+                                                    '???', 'country'])
 
 matches_2000 = pd.read_csv('atp_matches_2000.csv')
 matches_2001 = pd.read_csv('atp_matches_2001.csv')
@@ -30,7 +38,8 @@ matches_2015 = pd.read_csv('atp_matches_2015.csv')
 matches_2016 = pd.read_csv('atp_matches_2016.csv')
 matches_2017 = pd.read_csv('atp_matches_2017.csv')
 
-matches09_10 = pd.concat([matches_2009, matches_2010])
+#print len(list(matches_2017))
+#print matches_2000.iloc[645]
 
 matches00_17 = pd.concat([matches_2000, matches_2001, matches_2002,
                           matches_2003, matches_2004, matches_2005,
@@ -46,11 +55,11 @@ class H2H:
         self.player2ID = player2ID
 
     def player_record(self, matches_df):
-        p1winner = matches_df['winner_id'] == self.player1ID
-        p1loser = matches_df['loser_id'] == self.player1ID
+        p1winner = (matches_df['winner_id'] == self.player1ID)
+        p1loser = (matches_df['loser_id'] == self.player1ID)
 
-        p2winner = matches_df['winner_id'] == self.player2ID
-        p2loser = matches_df['loser_id'] == self.player2ID
+        p2winner = (matches_df['winner_id'] == self.player2ID)
+        p2loser = (matches_df['loser_id'] == self.player2ID)
 
         p1win_p2lose = matches_df[p1winner & p2loser]
         p1wins = p1win_p2lose.shape[0]
@@ -66,6 +75,47 @@ class H2H:
 
         return p1wins, p2wins, total_matches
 
+
 nadal_vs_federer = H2H(104745, 103819)
 
 print nadal_vs_federer.player_record(matches00_17)
+
+
+def number_of_games(playerID, matches_df):
+    '''
+    Calculates the total number of games a given player has competed in.
+    '''
+    wins = pd.DataFrame(matches_df['winner_id'] == playerID)
+    losses = pd.DataFrame(matches_df['loser_id'] == playerID)
+    total_matches = (wins.size + losses.size)
+    return total_matches
+
+print number_of_games(104745, matches00_17)
+
+#80th_percentile_players =
+
+
+H2H_df = pd.DataFrame(columns=['Player1', 'Player1Wins',
+                               'Player2', 'Player2Wins', 'TotalGames'])
+
+#print atp_players.head
+print atp_players[1:10]
+
+#for i in atp_players['player_id']:
+#    player1entries = []
+#    for j in atp_players['player_id']:
+#        if i == j:
+#            entry = ('NA', 'NA', 'NA', 'NA', 'NA')
+#        else:
+#            p1wins, p2wins, total_matches = H2H(i, j)
+#            entry = (i, p1wins, j, p2wins, total_matches)
+#            player1entries.append(entry)
+#
+#print player1entries
+
+# |---------|-------------|---------|-------------|------------|
+# | Player1 | Player1Wins | Player2 | Player2Wins | TotalGames |
+# |=========|=============|=========|=============|============|
+# |  Nadal  |      12     | Federer |      23     |     35     |
+# |---------|-------------|---------|-------------|------------|
+# |  Nadal  |      8      | Murray  |      8      |-----x+y----|
